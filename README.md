5bdbe453fadbb9ad06da0ebcb132b2d68f49bfe7
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-02-07 12:06:06 -0700

    added js multiply function

---
+++ README.md
@@ -0,0 +1,3 @@
+We are practicing the feature branch git workflow.
+
+Kevin Task 1:You should create an array of numbers in a Javascript file, if there isn't one. Write a funciton that multiplies them togehter.
---
+++ app.js
@@ -0,0 +1,10 @@
+const numbers = [3, -56, 31, 92, -4, 1, 3];
+
+function multiply(numberArray){
+  let total = numberArray.reduce(
+    (product, number) => {
+      return eval(product * number);
+    });
+  return total;
+} 
+multiply(numbers);
